ea6cc67b313735764329a8df387c2be0832e1da5
rogermedico <roger.medico@gmail.com>
2020-11-19 19:36:53 +0100

    js tweak and message sent alert

--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -9,6 +9,18 @@ import 'bootstrap';
  * Write any other JavaScript below
  */
 
+function iAmInHome(){
+  const path = window.location.pathname.split('/');
+  const actualPage = path[path.length-1];
+  if(['index.html',''].includes(actualPage)) return true;
+  else return false;
+}
+
+function ticketLinkTweak(){
+  addLinkClassToEntrades();
+  window.addEventListener('resize', addLinkClassToEntrades);
+}
+
 function addLinkClassToEntrades(){
   const mainTickets = document.querySelector('#home__tickets__link');
   if(mainTickets) {
@@ -17,7 +29,39 @@ function addLinkClassToEntrades(){
   }
 }
 
+function iAmInContact(){
+  const path = window.location.pathname.split('/');
+  const actualPage = path[path.length-1];
+  if('contact.html' == actualPage) return true;
+  else return false;
+}
+
+function formSubmited(){
+    const search = location.search.substring(1);
+    let params = {};
+    if(search) params = JSON.parse('{"' + decodeURI(search).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g,'":"') + '"}');
+    if(params.s == 1) return true;
+    else return false;
+}
+
+function displayAlert(){
+  const alert = document.querySelector('#alert_container');
+  alert.classList.add('container', 'alert', 'alert-dismissible', 'fadeIn', 'fade','show', 'col-10', 'offset-1', 'col-sm-8', 'offset-sm-2', 'col-lg-6', 'offset-lg-3', 'alert-success');
+  alert.setAttribute('role', 'alert');
+  alert.innerHTML = 'Message <strong>successfully</strong> sent';
+  const button = document.createElement('button');
+  button.type = 'button';
+  button.setAttribute('aria-label','Close');
+  button.setAttribute('data-dismiss','alert');
+  alert.appendChild(button);
+  const span = document.createElement('span');
+  span.setAttribute('aria-hidden','true');
+  span.innerHTML = '&times';
+  button.appendChild(span);
+  button.classList.add('close');
+}
+
 (() => {
-  addLinkClassToEntrades();
-  window.addEventListener('resize', addLinkClassToEntrades);
+  if(iAmInContact() && formSubmited()) displayAlert();
+  if(iAmInHome()) ticketLinkTweak();
 })();
